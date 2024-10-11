9e53bac8a2f9026684fea57393ea8dc7760a2462
hoangquan <hoangquan131102@gmail.com>
2024-10-11 11:37:33 +0700

    add main

--- untitled2/src/main/java/org/example/Main.java
+++ untitled2/src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ public class Main {
 
         System.out.printf("Hello and welcome12!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
